6df536afce7a79a6a345e9c70f284a288f8a94a1
HackerKid69 <119847769+HackerKid69@users.noreply.github.com>
2023-12-30 19:04:05 +1100

    Update downloads.js

--- downloads.js
+++ downloads.js
@@ -1,1 +1,7 @@
+function playQuackSound() {
+    const quackSounds = ['quack1.mp3', 'quack2.mp3', 'quack3.mp3', 'quack4.mp3', 'quack5.mp3', 'quack6.mp3'];
+    const randomQuack = quackSounds[Math.floor(Math.random() * quackSounds.length)];
 
+    const audio = new Audio(randomQuack);
+    audio.play();
+}
