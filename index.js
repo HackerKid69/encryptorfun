7ff57921e8308ec865c20c30424af7af57b0868a
HackerKid69 <119847769+HackerKid69@users.noreply.github.com>
2023-12-30 18:18:02 +1100

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 function encrypt() {
     const message = document.getElementById('message').value;
     const encryptedMessage = Array.from(message).map(char => char.charCodeAt(0)).join(',');
-    document.getElementById('result').innerText = `Encrypted: ${encryptedMessage}`;
+    document.getElementById('result').innerText = `🎉 Encrypt-o-ed: ${encryptedMessage}`;
 }
 
 function decrypt() {
     const unicodeString = document.getElementById('message').value;
     const unicodeArray = unicodeString.split(',').map(code => String.fromCharCode(code));
     const decryptedMessage = unicodeArray.join('');
-    document.getElementById('result').innerText = `Decrypted: ${decryptedMessage}`;
+    document.getElementById('result').innerText = `🎈 Decrypt-o-ed: ${decryptedMessage}`;
 }
