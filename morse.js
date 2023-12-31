8a92aff6ecc5dae3350f3401a37665d6462675a4
HackerKid69 <119847769+HackerKid69@users.noreply.github.com>
2023-12-31 12:48:33 +1100

    Update morse.js

--- morse.js
+++ morse.js
@@ -1,1 +1,23 @@
+function encodeMorse() {
+    const inputText = document.getElementById('morseInputText').value;
+    const encodedText = textToMorse(inputText);
+    document.getElementById('morseOutputText').value = 'Morse Code: ' + encodedText;
+}
 
+function decodeMorse() {
+    const inputText = document.getElementById('morseInputText').value;
+    const decodedText = morseToText(inputText);
+    document.getElementById('morseOutputText').value = 'Decoded Text: ' + decodedText;
+}
+
+function textToMorse(text) {
+    // Implement your text to Morse code conversion logic here
+    // Example: Replace this line with your implementation
+    return text.toUpperCase();
+}
+
+function morseToText(morseCode) {
+    // Implement your Morse code to text conversion logic here
+    // Example: Replace this line with your implementation
+    return morseCode.toLowerCase();
+}
