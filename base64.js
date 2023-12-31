4b330b4d3424aa49c0319f38f62a1aede2afbcf3
HackerKid69 <119847769+HackerKid69@users.noreply.github.com>
2023-12-31 12:31:19 +1100

    Update base64.js

--- base64.js
+++ base64.js
@@ -1,1 +1,20 @@
+// Function to encode the input text to Base64
+function encode() {
+    const inputText = document.getElementById('inputText').value;
+    const encodedText = btoa(inputText);
+    document.getElementById('result').innerText = 'Encoded Text: ' + encodedText;
+}
 
+// Function to decode the Base64 text
+function decode() {
+    const inputText = document.getElementById('inputText').value;
+    let decodedText;
+
+    try {
+        decodedText = atob(inputText);
+    } catch (error) {
+        decodedText = 'Invalid Base64 input';
+    }
+
+    document.getElementById('result').innerText = 'Decoded Text: ' + decodedText;
+}
