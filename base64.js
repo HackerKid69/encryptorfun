73c4668f62dafe6d17711c4ce94d47b3a882310a
HackerKid69 <119847769+HackerKid69@users.noreply.github.com>
2023-12-31 12:42:14 +1100

    Update base64.js

--- base64.js
+++ base64.js
@@ -1,61 +1,42 @@
 // Function to encode the input text to Base64
-function encode() {
-    // Step 1: Get the input text from the 'inputText' textarea
+function encodeBase64() {
     const inputText = document.getElementById('inputText').value;
-
-    // Step 2: Encode the input text to Base64 using the btoa function
     const encodedText = btoa(inputText);
-
-    // Step 3: Display the encoded text in the output textarea
-    document.getElementById('outputText').value = 'Encoded Text: ' + encodedText;
+    document.getElementById('outputText').value = 'Base64 Encoded Text: ' + encodedText;
 }
 
 // Function to decode the Base64 text
-function decode() {
-    // Step 1: Get the input text from the 'inputText' textarea
+function decodeBase64() {
     const inputText = document.getElementById('inputText').value;
-
     let decodedText;
 
-    // Step 2: Try to decode the Base64 text using the atob function
     try {
         decodedText = atob(inputText);
     } catch (error) {
-        // Step 3: Handle an error if the input is not valid Base64
         decodedText = 'Invalid Base64 input';
     }
 
-    // Step 4: Display the decoded text in the output textarea
-    document.getElementById('outputText').value = 'Decoded Text: ' + decodedText;
+    document.getElementById('outputText').value = 'Base64 Decoded Text: ' + decodedText;
 }
+
 // Function to encode the input text to Base32
 function encodeBase32() {
-    // Step 1: Get the input text from the 'inputText' textarea
     const inputText = document.getElementById('inputText').value;
-
-    // Step 2: Encode the input text to Base32 using a custom function
     const encodedText = customBase32Encode(inputText);
-
-    // Step 3: Display the encoded text in the output textarea
     document.getElementById('outputText').value = 'Base32 Encoded Text: ' + encodedText;
 }
 
 // Function to decode the Base32 text
 function decodeBase32() {
-    // Step 1: Get the input text from the 'inputText' textarea
     const inputText = document.getElementById('inputText').value;
-
     let decodedText;
 
-    // Step 2: Decode the Base32 text using a custom function
     try {
         decodedText = customBase32Decode(inputText);
     } catch (error) {
-        // Step 3: Handle an error if the input is not valid Base32
         decodedText = 'Invalid Base32 input';
     }
 
-    // Step 4: Display the decoded text in the output textarea
     document.getElementById('outputText').value = 'Base32 Decoded Text: ' + decodedText;
 }
 
